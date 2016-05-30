93f62623b70b8edf13a0fcb68ccf11db820e49a2
guojiaqi1027 <gawain_rutgers@yahoo.com>
2016-05-30 04:01:31 -0700

    Add service type

--- src/main/java/com/jiaqiguo/share/service/ServiceFactory.java
+++ src/main/java/com/jiaqiguo/share/service/ServiceFactory.java
@@ -2,6 +2,7 @@ package com.jiaqiguo.share.service;
 
 public class ServiceFactory {
 	public static final int CREATE_USER_SERVICE=0;
+	public static final int QUERY_USER_SHARES=1;
 	public Service createService(int serviceType){
 		switch(serviceType){
 		case CREATE_USER_SERVICE:
